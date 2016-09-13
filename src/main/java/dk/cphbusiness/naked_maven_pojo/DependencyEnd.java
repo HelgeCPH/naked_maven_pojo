122dd23c26bed81e70a60e513d427eb9336e8674
rhp <rhp@10.50.169.24>
2016-09-13 11:21:16 +0200

    Small example project, which is used for build system testing.

---
+++ src/main/java/dk/cphbusiness/naked_maven_pojo/DependencyEnd.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dk.cphbusiness.naked_maven_pojo;
+
+/**
+ *
+ * @author rhp
+ */
+public class DependencyEnd {
+
+    public DependencyEnd() {
+    }
+    
+    public String getDependencyEnd() {
+        return "End";
+    }
+}
